30d7350ec522e187545238d6bdaeb6d0400aa50b
katebouis <78283299+katebouis@users.noreply.github.com>
2022-01-25 10:04:59 -0600

    build badge in readme

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Kate Bouis on 1/24/22.
+//
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if(argc != 1) {
+        std::cout << "idk what's up here" << std::endl;
+    }
+    else {
+        std::cout << "will this build" << std::endl;
+    }
+    return 0;
+}
